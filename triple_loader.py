2aeebf42bbf62e073b79bb5bc28bd9ff41723519
brurucy <rucy.carneiro@pipedrive.com>
2022-03-20 22:43:31 +0200

    Fixed a bug.

--- triple_loader.py
+++ triple_loader.py
@@ -1,10 +1,11 @@
+from shlex import split
 
 
-def read_triples(file_name: str) -> list[tuple[int, int, int]]:
+def read_triples(file_name: str) -> list[tuple[str, str, str]]:
     triple_store = []
     with open(file_name, 'rb') as f:
         for line in f:
-            (s, p, o, dot) = line.split(None)
+            (s, p, o, dot) = split(line.decode("utf-8"))
             triple_store.append((s, p, o))
 
     return triple_store

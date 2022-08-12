49910f5d8310fbf32b4e21d1179e0aed02883524
brurucy <rucy.carneiro@pipedrive.com>
2022-08-12 17:43:14 +0300

    Rough

--- triple_loader.py
+++ triple_loader.py
@@ -1,6 +1,16 @@
 from shlex import split
 
 
+def read_encoded_triples(file_name: str) -> set[tuple[int, int, int]]:
+    encoded_triple_store = set()
+    with open(file_name, 'rb') as f:
+        for line in f:
+            (s, p, o) = split(line.decode("utf-8"))
+            encoded_triple_store.add((int(s), int(p), int(o)))
+
+    return encoded_triple_store
+
+
 def read_triples(file_name: str) -> list[tuple[str, str, str]]:
     triple_store = []
     with open(file_name, 'rb') as f:
